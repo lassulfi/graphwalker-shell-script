d668bb29062f494e411ec4740ba297631b17bc31
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-09-10 20:23:54 -0300

    Feita a validação e instalação do java

--- graphwalker.sh
+++ graphwalker.sh
@@ -9,7 +9,7 @@ installdir="$HOME/graphwalker"
 clfilename="$installdir/graphwalker-cli-3.4.2.jar"
 
 #versao do arquivo para web
-webfilename="$installdir/graphwalker-cli-4.0.0-SNAPSHOT.jar"
+webfilename="$installdir/graphwalker-studio-4.0.0-SNAPSHOT.jar"
 
 if [ ! -d "$installdir" ]; then
     #cria o diretorio na pasta home
@@ -18,18 +18,48 @@ if [ ! -d "$installdir" ]; then
     #download do arquivo - versão terminal
     clversion=$(wget --output-document=$clfilename http://graphwalker.github.io/content/archive/graphwalker-cli-3.4.2.jar)
     #donwload do arquivo 0 versão web
-    webversion=$(wget --output-document=$webfilename --server-response https://bintray.com/graphwalker/graphwalker/download_file?file_path=SNAPSHOTS%2Fgraphwalker-cli-4.0.0-SNAPSHOT.jar)
+    webversion=$(wget --output-document=$webfilename https://bintray.com/graphwalker/graphwalker/download_file?file_path=SNAPSHOTS%2Fgraphwalker-cli-4.0.0-SNAPSHOT.jar)
 fi
 
 #Execução do arquivo
-echo "Qual modo de execução desejado?"
-echo
-echo "Para tela digite 1"
-echo "Para navegador digite 2"
-echo
-read -p "Modo: " modo
-while [ "$modo" -ge 3 ]; do
-    echo "Opção inválida. Tente novamente"
+iniciarAplicacao()
+{
+    echo "Qual modo de execução desejado?"
+    echo
+    echo "Para sair digite 0"
+    echo "Para modo linha de comando digite 1"
+    echo "Para navegador digite 2"
+    echo
     read -p "Modo: " modo
-done
+    while [ "$modo" -ge 3 ]; do
+        echo "Opção inválida. Tente novamente"
+        read -p "Modo: " modo
+    done
 
+    case $modo in
+        0) exit 1
+        ;;
+        1) read -p "Arquivos: " arquivos
+            java -jar $clfilename offline -m $arquivos "random(edge_coverage(100))" -u
+        ;;
+        2) java -jar $webfilename
+        ;;
+    esac
+}
+
+#Instalacao do java 8
+instalarJava()
+{
+    #Adicionado ao repositorio
+    sudo add-apt-respository ppa:webupd8team/java
+    sudo apt-get update
+    sudo apt-get install oracle-java8-installer
+}
+
+#valida se o java está instalado
+if [[ -n $(type -p java) ]]; then
+    iniciarAplicacao
+else
+    instalarJava
+    iniciarAplicacao
+fi
